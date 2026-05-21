d4909f0afd43afef445a61462f0e8823bef4c417
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2026-05-21 09:21:47 +0200

    test(snap): split base container from spread images
    Each spread image was pre-provisioned with the default openshell container.
    This worked but the container is rather large and this would only work
    locally where disk space is effectively free. In GitHub actions, we should
    be mindful of the cache size and having one shared element cached across
    several test jobs is better.
    
    This requires a new version of Image Garden (to be released as 0.5.1).
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- .image-garden.mk
+++ .image-garden.mk
@@ -1,13 +1,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-default_sandbox_image=ghcr.io/nvidia/openshell-community/sandboxes/base:latest
-
 define install_and_setup_docker
 - addgroup --system docker
 - snap install docker
-- while [ ! -S /run/docker.sock ]; do sleep 3; done
-- "snap run docker pull $(default_sandbox_image)"
 endef
 
 define UBUNTU_CLOUD_INIT_USER_DATA_TEMPLATE
